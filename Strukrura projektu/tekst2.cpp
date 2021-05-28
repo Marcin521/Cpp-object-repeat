1546736f5bfc4353c2df52780019a97d0308ba1b
Marcin521 <marcinkondo134@gmail.com>
2021-05-28 21:33:46 +0200

    add files to project .h .cpp

---
+++ Strukrura projektu/tekst2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Marcin on 28.05.2021.
+//
+
+#include "tekst2.h"
---
+++ Strukrura projektu/tekst2.h
@@ -0,0 +1,14 @@
+//
+// Created by Marcin on 28.05.2021.
+//
+
+#ifndef C_OBJE_POWT_TEKST2_H
+#define C_OBJE_POWT_TEKST2_H
+
+
+class pytania {
+
+};
+
+
+#endif //C_OBJE_POWT_TEKST2_H
