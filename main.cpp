9522d080339245149966460a0a935676b384a15d
Marcin521 <marcinkondo134@gmail.com>
2021-05-28 20:37:17 +0200

    add finish project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+using namespace std;
+
+class Animal{
+
+public:
+
+    string gatunek;
+    string imie;
+    int wiek;
+
+    void dodaj_zwierze(){
+
+        cout << "podaj gatunek" << endl;
+        cin >> gatunek;
+
+        cout << "podaj imie" << endl;
+        cin >> imie;
+
+        cout << "podaj wiek" << endl;
+        cin >> wiek;
+    }
+
+    void glos(){
+        if(gatunek=="krowa") cout << "muuuuuu " << "lat " << wiek;
+    }
+
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    //twoże obiekt o nazwie z1
+    Animal z1;
+
+    //wywołanie metody z klasy
+    z1. dodaj_zwierze();
+    z1. glos();
+
     return 0;
 }
