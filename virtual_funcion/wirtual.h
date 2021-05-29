a0334109f800d98cbf153606b7afcc2ae65b2ff9
Marcin521 <marcinkondo134@gmail.com>
2021-05-29 21:27:20 +0200

    add new project

---
+++ virtual_funcion/wirtual.h
@@ -0,0 +1,14 @@
+//
+// Created by Marcin on 29.05.2021.
+//
+
+#ifndef VIRTUAL_FUNCION_WIRTUAL_H
+#define VIRTUAL_FUNCION_WIRTUAL_H
+
+
+class wirtual {
+
+};
+
+
+#endif //VIRTUAL_FUNCION_WIRTUAL_H
