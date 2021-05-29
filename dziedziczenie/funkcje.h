28238e2f61ef0180cd2a146169238037df5e65bd
Marcin521 <marcinkondo134@gmail.com>
2021-05-29 18:25:55 +0200

    add funkcej.h funkcje.cpp

---
+++ dziedziczenie/funkcje.h
@@ -0,0 +1,14 @@
+//
+// Created by Marcin on 29.05.2021.
+//
+
+#ifndef DZIEDZICZENIE_FUNKCJE_H
+#define DZIEDZICZENIE_FUNKCJE_H
+
+
+class funkcje {
+
+};
+
+
+#endif //DZIEDZICZENIE_FUNKCJE_H
--- dziedziczenie/main.cpp
+++ dziedziczenie/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "funkcje.h"
 
 using namespace std;
 
