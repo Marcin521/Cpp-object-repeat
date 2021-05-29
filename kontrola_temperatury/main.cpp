30c6591e904b80a2effafa4d6e53c0e7f18c2a3e
Marcin521 <marcinkondo134@gmail.com>
2021-05-29 09:33:57 +0200

    add 2 second waint

--- kontrola_temperatury/main.cpp
+++ kontrola_temperatury/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <time.h>
+#include <cstdlib>
+
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+int ile, temp, next;
+float czas;
+clock_t start, stop;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+
+    int *odczyt;
+    odczyt = &temp;
+
+    int *tablica;
+    tablica= new int [ile];
+
+    ile = 1000000000;
+    while(1==1){
+
+
+        for (int i=1; i>0; i++){
+
+            chrono::seconds interval( 2 );
+
+                tablica[i]=i;
+            cout << tablica[i] << endl;
+
+            this_thread::sleep_for( interval ) ;
+
+
+        }
+
+        if(temp==000)   return 0;
+    }
+
+
+
+    delete [] tablica;
+
+    cout << "Hello, World!" << endl;
+
 }
